c8f585d785d5665a418dcdc81bc78948d183f8cc
Chelsea Weber <chelseaweber92@gmail.com>
2018-01-16 18:25:02 -0500

    first tutorial

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
+// declarative rendering
 var app = new Vue({
 	el: '#app',
 	data: {
 		message: 'Hello Vue!'
 	}
+})
+
+// data binding
+var app2 = new Vue({
+	el: '#app-2',
+	data: {
+		message: 'You loaded this page on ' + new Date().toLocaleDateString()
+	}
+})
+
+// conditionals 
+var app3 = new Vue({
+	el: '#app-3',
+	data: {
+		seen: true
+	}
+})
+
+// loops
+var app4 = new Vue({
+	el: '#app-4',
+	data: {
+		todos: [
+			{ text: 'learn Vue' },
+			{ text: 'learn React' },
+			{ text: 'learn a backend language' }
+		]
+	}
+	
+	
+})
+
+// user interaction
+var app5 = new Vue({
+	el: '#app-5',
+	data: {
+		message: 'Hello Vue'
+	},
+	methods: {
+		reverseMessage: function() {
+			this.message = this.message.split('').reverse().join('')
+		}
+	}
+})
+
+var app6 = new Vue({
+	el: '#app-6',
+	data: {
+		message: 'Hello Vue'
+	}
+})
+
+// components
+Vue.component('todo-item', {
+	props: ['todo'],
+	template: '<li>{{ todo.text }}</li>'
 })
